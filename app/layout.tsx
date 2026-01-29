41b350dcf0a530d9e0b389f3c48eea58e6afc886
Vansh Chouhan <dragonnbutded@gmail.com>
2026-01-30 04:03:12 +0530

    feat: CD with rotation motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Neuton } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const neuton = Neuton({
+  variable: "--font-neuton",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["200", "300", "400", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${neuton.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,17 @@
-import Image from "next/image";
+"use client";
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-120 h-120 bg-[#D1CFD2] rounded-[5rem] border border-[#CCCACC] overflow-hidden relative z-0 flex items-center justify-center">
+        <motion.div initial={{rotate: 0}} animate={{rotate: 360}} transition={{duration: 20, ease: "easeInOut"}} className="w-full h-full absolute flex items-center justify-center -top-64">
+          <div className="w-full h-full rounded-full absolute bg-[url('https://static.qobuz.com/images/covers/ga/qh/rdlgxy158qhga_600.jpg')] bg-cover bg-center border-2 border-gray-600 shadow-[0_35px_45px_5px_rgba(0,0,0,0.25)]" />
+          <div className="w-1/4 h-1/4 bg-[#878B96] border-2 border-gray-100 rounded-full absolute shadow-[0px_10px_15px_2px_rgba(0,0,0,0.25)]" />
+          <div className="w-21 h-21 rounded-full bg-[#BCB9BE] absolute" />
+          <div className="w-16 h-16 bg-[#D9D7D9] border-4 border-[#DEDBE0] rounded-full absolute shadow-[inset_0_0px_5px_2px_rgba(0,0,0,0.25)]" />
+        </motion.div>
+      </div>
     </div>
   );
 }
