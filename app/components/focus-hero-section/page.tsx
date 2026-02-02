5d9a5c122bb3dc2ae2e9308048f3cd8670413f88
Vansh Chouhan <dragonnbutded@gmail.com>
2026-02-02 21:29:36 +0530

    feat: Background for Hero section

---
+++ app/components/focus-hero-section/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="w-full h-screen bg-black">
+      <div className="w-full h-full bg-[url(/bg-1.jpg)] bg-center bg-cover opacity-50"></div>
+    </div>
+  );
+}
